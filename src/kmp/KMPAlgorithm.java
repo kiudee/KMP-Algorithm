a6865b50b05edf3aa594e059c409c34dc06d2a03
Karlson Pfannschmidt <quietdeath@gmail.com>
2011-01-27 12:27:31 +0100

    KMPAlgorithm: computePrefixFunction now working on char array

--- src/kmp/KMPAlgorithm.java
+++ src/kmp/KMPAlgorithm.java
@@ -2,25 +2,34 @@ package kmp;
 
 import utilities.SubString;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class KMPAlgorithm {
 
-    public void kmpMatcher(LinkedList<SubString> list, String P, String T, int currentShift) {
-        int m = P.length();
-        int n = T.length();
-        int[] func = computePrefixFunction(P);
-        int q = 0;
-        for (int i = 0; i < n; i++) {
-            while (q > 0 && P.charAt(q) != T.charAt(i))
-                q = func[q - 1];
-            if (P.charAt(q) == T.charAt(i))
-                q++;
-            if (q == m) {
-                list.add(new SubString(P, i - m + 1, currentShift));
-                q = func[q - 1];
-            }
+    private char[] string;
+    private char[] pattern;
+
+    public KMPAlgorithm(String string, String pattern) {
+        this.string = string.toCharArray();
+        this.pattern = pattern.toCharArray();
+    }
+
+    private KMPAlgorithm() {
+    }
+
+    private int[] computePrefixFunction(int shift, int length) {
+        final int[] func = new int[length];
+        func[0] = 0;
+        int k = shift;
+
+        for (int q = shift + 1; q < length + shift; q++) {
+            while (k > shift && pattern[k] != pattern[q])
+                k = func[k - 1 - shift] + shift;
+            if (pattern[k] == pattern[q]) k++;
+            func[q - shift] = k - shift;
         }
+        return func;
     }
 
     private int[] computePrefixFunction(String P) {
@@ -39,8 +48,27 @@ public class KMPAlgorithm {
 
     }
 
+    public void kmpMatcher(LinkedList<SubString> list, String P, String T, int currentShift) {
+        int m = P.length();
+        int n = T.length();
+        int[] func = computePrefixFunction(P);
+        int q = 0;
+        for (int i = 0; i < n; i++) {
+            while (q > 0 && P.charAt(q) != T.charAt(i))
+                q = func[q - 1];
+            if (P.charAt(q) == T.charAt(i))
+                q++;
+            if (q == m) {
+                list.add(new SubString(P, i - m + 1, currentShift));
+                q = func[q - 1];
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-        KMPAlgorithm kmp = new KMPAlgorithm();
-        kmp.computePrefixFunction("ababababca");
+        KMPAlgorithm kmp = new KMPAlgorithm("ababababca", "ababababca");
+        int[] pf = kmp.computePrefixFunction(1, 5);
+        System.out.println(Arrays.toString(pf));
     }
 }
--- src/kmp/KMPLCSAlgorithm.java
+++ src/kmp/KMPLCSAlgorithm.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 
 public class KMPLCSAlgorithm implements IAlgorithm {
     public String run(String T1, String T2) {
-        KMPAlgorithm kmp = new KMPAlgorithm();
+        KMPAlgorithm kmp = new KMPAlgorithm(T1, T2);
         String minString, maxString;
         if (T1.length() < T2.length()) {
             minString = T1;
